53336e85b54f524594c54ba332ae25088ba99590
Suraj <singh.suraj1025@gmail.com>
2023-06-12 15:10:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const redis = require("redis");
+const RedisStore = require("connect-redis").default;
+const cors = require("cors");
+
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+  REDIS_URL,
+  SESSION_SECRET,
+  REDIS_PORT,
+} = require("./config/config");
+
+const redisClient = redis.createClient({
+  url: `redis://${REDIS_URL}:${REDIS_PORT}`,
+});
+
+redisClient.connect().catch(console.error);
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
+
+const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Successfully connected to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+app.enable("trust proxy");
+app.use(cors({}));
+app.use(
+  session({
+    proxy: true,
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: false,
+
+      httpOnly: true,
+      maxAge: 1200000,
+    },
+  })
+);
+
+app.use(express.json());
+
+app.get("/api/v1", (req, res) => {
+  res.send("<h2>Hi There</h2>");
+  console.log("yeah it ran");
+});
+
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
